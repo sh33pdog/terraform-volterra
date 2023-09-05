5aa8e69f048002055d65d42d89ea1aa9aad5898f
sh33pdog <keiron.shepherd@gmail.com>
2023-09-05 10:55:27 +0100

    Update variables.tf

--- HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP/variables.tf
+++ HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP/variables.tf
@@ -1,14 +1,14 @@
 variable "api_cert" {
             type = string
-            default = "/PATH/certificate.cert"
+            default = "/Users/K.Shepherd/Github/f5xc_api_key/emea-ent/kshep-f5xc-cert.crt"
         }
         
         variable "api_key" {
           type = string
-          default = "/PATH/private_key.key"
+          default = "/Users/K.Shepherd/Github/f5xc_api_key/emea-ent/kshep-f5xc-key.key"
         }
         
         variable "api_url" {
             type = string
-            default = "https://YOUT_TENANT.console.ves.volterra.io/api"
+            default = "https://f5-emea-ent.console.ves.volterra.io/api"
         }
